09c7868f8a4e1fe6a975a773262b769575673f23
Ludo <lud0@users.noreply.github.com>
2020-11-16 18:57:35 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         "requests >= 2.24.0",
-        "setuptools >= 39.0.1"
+        "setuptools >= 39.0.1",
         "urllib3 >= 1.26.2"
     ],
     packages=[
